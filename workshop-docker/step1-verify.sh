a972573d63aeb65944e502ff1d08ecad221d56ba
Dirceu Silva <dirceu@instruct.com.br>
2018-12-07 12:14:47 -0200

    v0.0.21 - Validando o primeiro

--- workshop-docker/step1-verify.sh
+++ workshop-docker/step1-verify.sh
@@ -1,1 +1,1 @@
-(( $(grep "Trata-se um exemplo de texto. Copie e cole na IDE (no arquivo teste-terminal.txt)" teste-terminal.txt 2>/dev/null | wc -l) >= 1)) && echo \"done\"
+(( $(grep "Trata-se um exemplo de texto" teste-terminal.txt 2>/dev/null | wc -l) >= 1)) && echo \"done\"
